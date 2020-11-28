c2127b69d1781188eba204fe3af46b163644df34
EnteiCross <luisbayliss@outlook.com>
2020-11-28 13:20:35 -0600

    Se corrigue sentencia 'writeHeader' por 'writeHead', ademas colocar bien el segundo paramentro del Content-Type.

--- 02ModulosNPM/00Globales/02-Monitor.js
+++ 02ModulosNPM/00Globales/02-Monitor.js
@@ -13,8 +13,7 @@ function webServer(req, res) {
         res.end(data);
     }
 
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
+    res.writeHead(200, {'Content-Type': 'text/plain'});
     fs.readFile('./assets/index.html', readFile);
 }
 
